fe3cc6bc5e7dbba8d421b7ad466ea6a78a0a848e
Jechf <jesuse.chf@gmail.com>
2022-09-15 16:21:40 +0000

    private route

--- src/front/js/pages/private.js
+++ src/front/js/pages/private.js
@@ -1,5 +1,24 @@
 import React from "react";
 
 export const Private = () => {
-  return <div>Esta es una pagina Private</div>;
+  return (
+    <div className="text-center">
+      <div className="row">
+        <div className="col-3"></div>
+        <div className="col-6 mt-1 p-2">
+          <h3 className="mb-2 p-1">Implementación de la autenticación JWT</h3>
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/8-W2O_R95Pk"
+            title="YouTube video player"
+            frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen
+          ></iframe>
+        </div>
+        <div className="col-3 mb-3"></div>
+      </div>
+    </div>
+  );
 };
